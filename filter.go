5ad8f0902e0d31d046e28feb4fca461272d952f3
Steffen Brüheim <ueffel@gmail.com>
2021-12-05 20:38:06 +0100

    initial commit

---
+++ filter.go
@@ -0,0 +1,52 @@
+package basic_auth_filter
+
+import (
+	"net/http"
+
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
+	"github.com/caddyserver/caddy/v2/modules/logging"
+	"go.uber.org/zap/zapcore"
+)
+
+type BasicAuthFilter struct{}
+
+// CaddyModule returns the Caddy module information.
+func (BasicAuthFilter) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID:  "caddy.logging.encoders.filter.basic_auth_user",
+		New: func() caddy.Module { return new(BasicAuthFilter) },
+	}
+}
+
+func (f *BasicAuthFilter) Filter(in zapcore.Field) zapcore.Field {
+	authHeader, ok := in.Interface.(caddyhttp.LoggableStringArray)
+	if !ok {
+		return in
+	}
+	fakeReq := &http.Request{Header: http.Header{"Authorization": authHeader}}
+	userID, _, ok := fakeReq.BasicAuth()
+	in.Type = zapcore.StringType
+	if ok {
+		in.String = userID
+	} else {
+		in.String = ""
+	}
+	return in
+}
+
+// UnmarshalCaddyfile sets up the module from Caddyfile tokens.
+func (*BasicAuthFilter) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
+	return nil
+}
+
+func init() {
+	caddy.RegisterModule(BasicAuthFilter{})
+}
+
+// Interface guards.
+var (
+	_ logging.LogFieldFilter = (*BasicAuthFilter)(nil)
+	_ caddyfile.Unmarshaler  = (*BasicAuthFilter)(nil)
+)
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/ueffel/caddy-basic-auth-filter
+
+go 1.16
+
+require (
+	github.com/caddyserver/caddy/v2 v2.4.0
+	go.uber.org/zap v1.16.0
+)
